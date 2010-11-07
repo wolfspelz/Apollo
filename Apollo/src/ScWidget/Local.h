f6e295a29663d68f5343eb1cff7602fee6469875
Heiner Wolf <wolfspelz@users.noreply.github.com>
2010-11-07 18:18:15 +0000

    ScWidget

---
+++ Apollo/src/ScWidget/Local.h
@@ -0,0 +1,13 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#if !defined(Local_H_INCLUDED)
+#define Local_H_INCLUDED
+
+#define MODULE_NAME "ScWidget"
+#define LOG_CHANNEL MODULE_NAME
+
+#endif // Local_H_INCLUDED
---
+++ Apollo/src/ScWidget/ScWidgetModule.h
@@ -0,0 +1,46 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#if !defined(ScWidgetModule_H_INCLUDED)
+#define ScWidgetModule_H_INCLUDED
+
+#include "ApModule.h"
+#include "ApContainer.h"
+#include "MsgUnitTest.h"
+#include "MsgScWidget.h"
+#include "ScWidgetModuleTester.h"
+
+class ScWidgetModule
+{
+public:
+  ScWidgetModule()
+    {}
+
+  int Init();
+  void Exit();
+
+  //void On_ScWidget_Create(Msg_ScWidget_Create* pMsg);
+
+#if defined(AP_TEST)
+  void On_UnitTest_Begin(Msg_UnitTest_Begin* pMsg);
+  void On_UnitTest_Execute(Msg_UnitTest_Execute* pMsg);
+  void On_UnitTest_End(Msg_UnitTest_End* pMsg);
+#endif
+
+protected:
+
+public:
+
+  AP_MSG_REGISTRY_DECLARE;
+
+#if defined(AP_TEST)
+  friend class ScWidgetModuleTester;
+#endif
+};
+
+typedef ApModuleSingleton<ScWidgetModule> ScWidgetModuleInstance;
+
+#endif // ScWidgetModule_H_INCLUDED
