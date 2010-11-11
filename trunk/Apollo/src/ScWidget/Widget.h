5711381c114d622b3d864039736e82d60f6f562b
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2010-11-11 23:45:38 +0000

    Widget: Button

---
+++ trunk/Apollo/src/ScWidget/Widget.h
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#if !defined(Widget_H_INCLUDED)
+#define Widget_H_INCLUDED
+
+#include "MsgScene.h"
+
+class Button;
+
+class Widget
+{
+public:
+  Widget(const ApHandle& hScene, const String& sPath)
+    :hScene_(hScene)
+    ,sPath_(sPath)
+  {}
+  virtual ~Widget() {}
+
+  inline int IsButton() { return 0; }
+
+  virtual void Delete() {}
+  virtual void SetCoordinates(double fX, double fY, double fW, double fH);
+  Button* AsButton();
+
+  virtual void OnMouseEvent(Msg_Scene_MouseEvent* pMsg) {}
+
+protected:
+  ApHandle hScene_;
+  String sPath_;
+
+  double fX_;
+  double fY_;
+  double fW_;
+  double fH_;
+};
+
+#endif // Widget_H_INCLUDED
