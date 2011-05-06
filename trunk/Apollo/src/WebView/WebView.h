2079a492ef1b77bf7502c7c04a029374dcb683cd
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2011-05-06 13:44:07 +0000

    WebView 2nd

---
+++ trunk/Apollo/src/WebView/WebView.h
@@ -0,0 +1,44 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#if !defined(WebView_H_INCLUDED)
+#define WebView_H_INCLUDED
+
+#include <WebKit/WebKit.h>
+#include <WebKit/WebKitCOMAPI.h>
+
+class WebView
+{
+public:
+  WebView(const ApHandle& hWebView);
+  virtual ~WebView();
+
+  inline ApHandle apHandle() { return hAp_; }
+
+  int Create(const String& sHtml, const String& sBase);
+  void Destroy();
+  void SetPosition(int nX, int nY, int nW, int nH);
+  void SetVisibility(int bVisible);
+
+protected:
+  ApHandle hAp_;
+  int bVisible_;
+  int nX_;
+  int nY_;
+  int nW_;
+  int nH_;
+
+  IWebView* pWebKit_;
+#if defined(WIN32)
+  HWND hWnd_;
+#endif // WIN32
+
+#if defined(AP_TEST)
+  friend class WebViewModuleTester;
+#endif
+};
+
+#endif // WebView_H_INCLUDED
