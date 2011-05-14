71c299a57909e25863e91ed50ce42fa3017264a1
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2011-05-14 23:21:36 +0000

    WebArena -> Arena

---
+++ trunk/Apollo/src/Arena/Display.cpp
@@ -0,0 +1,432 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#include "Apollo.h"
+#include "MsgVpView.h"
+#include "ColorString.h"
+#include "Local.h"
+#include "WebArenaModule.h"
+#include "Display.h"
+#include "Avatar.h"
+
+Display::Display(WebArenaModule* pModule, const ApHandle& hContext)
+:pModule_(pModule)
+,hContext_(hContext)
+,bVisible_(0)
+,nLeft_(0)
+,nBottom_(0)
+,nWidth_(0)
+,nHeight_(0)
+{
+}
+
+Display::~Display()
+{
+}
+
+int Display::Create()
+{
+  int ok = 0;
+
+  ApHandle hView = Apollo::newHandle();
+
+  ok = Msg_WebView_Create::_(hView);
+  if (!ok) { apLog_Error((LOG_CHANNEL, "Display::Create", "Msg_WebView_Create(" ApHandleFormat ") failed", ApHandleType(hView))); }
+
+  if (ok) {
+    ok = Msg_WebView_SetScriptAccessPolicy::Allow(hView);
+    if (!ok) { apLog_Error((LOG_CHANNEL, "Display::Create", "Msg_WebView_SetScriptAccessPolicy::Allow(" ApHandleFormat ") failed", ApHandleType(hView))); }
+  }
+
+  if (ok) {
+    ok = Msg_WebView_Load::_(hView, Apollo::getModuleConfig(MODULE_NAME, "DisplayHtml", "file://" + Apollo::getModuleResourcePath(MODULE_NAME) + "arena.html"));
+    if (!ok) { apLog_Error((LOG_CHANNEL, "Display::Create", "Msg_WebView_Load(" ApHandleFormat ") failed", ApHandleType(hView))); }
+  }
+
+  if (ok) {
+    ok = Msg_WebView_SetNavigationPolicy::Deny(hView);
+    if (!ok) { apLog_Error((LOG_CHANNEL, "Display::Create", "Msg_WebView_SetNavigationPolicy::Deny(" ApHandleFormat ") failed", ApHandleType(hView))); }
+  }
+
+  if (ok) {
+    hView_ = hView;
+    pModule_->SetContextOfHandle(hView_, hContext_);
+
+    { Msg_VpView_SubscribeContextDetail msg; msg.hContext = hContext_; msg.sKey = Msg_VpView_ContextDetail_DocumentUrl; msg.Request(); }
+    { Msg_VpView_SubscribeContextDetail msg; msg.hContext = hContext_; msg.sKey = Msg_VpView_ContextDetail_LocationUrl; msg.Request(); }
+
+    //{ Msg_VpView_GetContextDetail msg; msg.hContext = hContext_; msg.sKey = Msg_VpView_ContextDetail_DocumentUrl; if (msg.Request()) { Meta* p = GetMeta(); if (p) { p->OnDocumentUrl(msg.sValue); }} }
+    //{ Msg_VpView_GetContextDetail msg; msg.hContext = hContext_; msg.sKey = Msg_VpView_ContextDetail_LocationUrl; if (msg.Request()) { Meta* p = GetMeta(); if (p) { p->OnLocationUrl(msg.sValue); }} }
+  }
+
+  return ok;
+}
+
+void Display::Destroy()
+{
+  pModule_->DeleteContextOfHandle(hView_, hContext_);
+
+  Msg_WebView_Destroy msg;
+  msg.hView = hView_;
+  if (!msg.Request()) {
+    apLog_Error((LOG_CHANNEL, "Display::Destroy", "Msg_WebView_Destroy(" ApHandleFormat ") failed", ApHandleType(msg.hView)));
+  }
+}
+
+void Display::SetVisibility(int bVisible)
+{
+  bVisible_ = bVisible;
+
+  Msg_WebView_Visibility msg;
+  msg.hView = hView_;
+  msg.bVisible = bVisible_;
+  if (!msg.Request()) {
+    apLog_Error((LOG_CHANNEL, "Display::SetPosition", "Msg_WebView_Visibility(" ApHandleFormat ") failed", ApHandleType(msg.hView)));
+  }
+}
+
+void Display::SetPosition(int nX, int nY)
+{
+  nLeft_ = nX;
+  nBottom_ = nY;
+
+  SendPosition();
+}
+
+void Display::SetSize(int nW, int nH)
+{
+  nWidth_ = nW;
+  nHeight_ = nH;
+
+  SendPosition();
+}
+
+void Display::SendPosition()
+{
+  Msg_WebView_Position msg;
+  msg.hView = hView_;
+  msg.nX = nLeft_;
+  msg.nY = nBottom_ - nHeight_;
+  msg.nW = nWidth_;
+  msg.nH = nHeight_;
+  if (!msg.Request()) {
+    apLog_Error((LOG_CHANNEL, "Display::SendPosition", "Msg_WebView_Position(" ApHandleFormat ") failed", ApHandleType(msg.hView)));
+  }
+}
+
+//---------------------------
+
+void Display::AttachLocation(const ApHandle& hLocation)
+{
+  hLocation_ = hLocation;
+
+  //if (pLocationInfo_) {
+  //  pLocationInfo_->SetLocation(hLocation);
+  //}
+
+  if (pModule_) {
+    pModule_->SetContextOfHandle(hLocation, hContext_);
+  }
+}
+
+void Display::DetachLocation(const ApHandle& hLocation)
+{
+  if (pModule_) {
+    pModule_->DeleteContextOfHandle(hLocation, hContext_);
+  }
+
+  RemoveAllAvatars();
+  RemoveAllObjects();
+  ResetLocationInfo();
+
+  if (hLocation_ == hLocation) {
+    hLocation_ = ApNoHandle;
+  }
+}
+
+//---------------------------
+
+void Display::OnEnterRequested()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnEnterRequested", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  tvEnterRequested_ = Apollo::TimeValue::getTime();
+
+  DisplaySrpcMessage dsm(this, "SetLocationState");
+  dsm.srpc.setString("sState", "EnterRequested");
+  dsm.Request();
+}
+
+void Display::OnEnterBegin()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnEnterBegin", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  tvEnterBegin_ = Apollo::TimeValue::getTime();
+
+  DisplaySrpcMessage dsm(this, "SetLocationState");
+  dsm.srpc.setString("sState", "EnterBegin");
+  dsm.Request();
+}
+
+void Display::OnEnterComplete()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnEnterComplete", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  {
+    ApAsyncMessage<Msg_VpView_ReplayLocationPublicChat> msg;
+    msg->hLocation = hLocation_;
+    //msg->nMaxAge;
+    //msg->nMaxLines;
+    //msg->nMaxData;
+    msg->PostAsync();
+  }
+
+  DisplaySrpcMessage dsm(this, "SetLocationState");
+  dsm.srpc.setString("sState", "EnterComplete");
+  dsm.Request();
+}
+
+void Display::OnLeaveRequested()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnLeaveRequested", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  tvLeaveRequested_ = Apollo::TimeValue::getTime();
+
+  DisplaySrpcMessage msg(this, "SetLocationState");
+  msg.srpc.setString("sState", "LeaveRequested");
+  msg.Request();
+}
+
+void Display::OnLeaveBegin()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnLeaveBegin", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  tvLeaveBegin_ = Apollo::TimeValue::getTime();
+
+  DisplaySrpcMessage dsm(this, "SetLocationState");
+  dsm.srpc.setString("sState", "LeaveBegin");
+  dsm.Request();
+}
+
+void Display::OnLeaveComplete()
+{
+  apLog_Verbose((LOG_CHANNEL, "Display::OnLeaveComplete", "ctxt=" ApHandleFormat " loc=" ApHandleFormat "", ApHandleType(hContext_), ApHandleType(hLocation_)));
+
+  DisplaySrpcMessage dsm(this, "SetLocationState");
+  dsm.srpc.setString("sState", "LeaveComplete");
+  dsm.Request();
+}
+
+//---------------------------------------------------
+
+void Display::OnReceivePublicChat(const ApHandle& hParticipant, const ApHandle& hChat, const String& sNickname, const String& sText, const Apollo::TimeValue& tv)
+{
+  AvatarListNode* pNode = avatars_.Find(hParticipant);
+  if (pNode) {
+    Avatar* pAvatar = pNode->Value();
+    if (pAvatar) {
+      pAvatar->OnReceivePublicChat(hChat, sNickname, sText, tv);
+    }
+  }
+}
+
+void Display::OnParticipantDetailsChanged(const ApHandle& hParticipant, Apollo::ValueList& vlKeys)
+{
+  AvatarListNode* pNode = avatars_.Find(hParticipant);
+  if (pNode) {
+    Avatar* pAvatar = pNode->Value();
+    if (pAvatar) {
+      pAvatar->OnDetailsChanged(vlKeys);
+    }
+  }
+}
+
+void Display::OnAvatarAnimationBegin(const ApHandle& hParticipant, const String& sUrl)
+{
+  AvatarListNode* pNode = avatars_.Find(hParticipant);
+  if (pNode) {
+    Avatar* pAvatar = pNode->Value();
+    if (pAvatar) {
+      pAvatar->OnAnimationBegin(sUrl);
+    }
+  }
+}
+
+void Display::OnCallModuleSrpc(Apollo::SrpcMessage& request, Apollo::SrpcMessage& response)
+{
+  String sMethod = request.getString("Method");
+
+  if (0){
+  } else if (sMethod == "SendPublicChat") {
+    String sText = request.getString("sText");
+    if (sText){
+      Msg_Vp_SendPublicChat msg;
+      msg.hLocation = hLocation_;
+      msg.sText = sText;
+      if (!msg.Request()) { throw ApException("Msg_Vp_SendPublicChat failed  loc=" ApHandleFormat "", ApHandleType(hLocation_)); }
+    }
+
+  } else {
+    throw ApException("Unknown Method=%s", StringType(sMethod));
+  }
+}
+
+//---------------------------------------------------
+
+void Display::OnParticipantsChanged()
+{
+  Msg_VpView_GetParticipants msg;
+  msg.hLocation = hLocation_;
+  if (msg.Request()) {
+    ProcessAvatarList(msg.vlParticipants, msg.hSelf);
+  }
+}
+
+void Display::ProcessAvatarList(Apollo::ValueList& vlParticipants, ApHandle& hSelf)
+{
+  hSelf_ = hSelf;
+
+  ParticipantFlags addedParticipants;
+  ParticipantFlags removedParticipants;
+
+  InitRemovedParticipants(removedParticipants);
+  InitAddedParticipants(addedParticipants);
+  EvaluateNewAvatarList(vlParticipants, addedParticipants, removedParticipants);
+  ProcessRemovedParticipants(removedParticipants);
+  ProcessAddedParticipants(addedParticipants);
+}
+
+void Display::InitRemovedParticipants(ParticipantFlags& removedParticipants)
+{
+  while (removedParticipants.Count() >0) {
+    ApHandleTreeNode<int>* pNode = removedParticipants.Next(0);
+    if (pNode) {
+      removedParticipants.Unset(pNode->Key());
+    }
+  }
+
+  AvatarListIterator iter(avatars_);
+  for (AvatarListNode* pNode = 0; (pNode = iter.Next()) != 0; ) {
+    ApHandle hAvatar = pNode->Key();
+    removedParticipants.Set(hAvatar, 1);
+  }
+}
+
+void Display::InitAddedParticipants(ParticipantFlags& addedParticipants)
+{
+  while (addedParticipants.Count() >0) {
+    ApHandleTreeNode<int>* pNode = addedParticipants.Next(0);
+    if (pNode) {
+      addedParticipants.Unset(pNode->Key());
+    }
+  }
+}
+
+void Display::EvaluateNewAvatarList(Apollo::ValueList& vlParticipants, ParticipantFlags& addedParticipants, ParticipantFlags& removedParticipants)
+{
+  for (Apollo::ValueElem* e = 0; e = vlParticipants.nextElem(e); ) {
+    AvatarListNode* pNode = avatars_.Find(e->getHandle());
+    if (pNode) {
+      RemoveFromRemovedParticipants(e->getHandle(), removedParticipants);
+    } else {
+      AddToAddedParticipants(e->getHandle(), addedParticipants);
+    }
+  }
+}
+
+void Display::RemoveFromRemovedParticipants(const ApHandle& h, ParticipantFlags& removedParticipants)
+{
+  removedParticipants.Unset(h);
+}
+
+void Display::AddToAddedParticipants(const ApHandle& h, ParticipantFlags& addedParticipants)
+{
+  addedParticipants.Set(h, 1);
+}
+
+void Display::ProcessAddedParticipants(ParticipantFlags& addedParticipants)
+{
+  ApHandleTreeIterator<int> iter(addedParticipants);
+  for (ApHandleTreeNode<int>* pNode = 0; (pNode = iter.Next()) != 0; ) {
+    ApHandle hParticipant = pNode->Key();
+    Avatar* pAvatar = new Avatar(pModule_, this, hParticipant);
+    if (pAvatar) {
+      avatars_.Set(hParticipant, pAvatar);
+      pAvatar->Create(hParticipant == hSelf_);
+    }
+  }
+}
+
+void Display::ProcessRemovedParticipants(ParticipantFlags& removedParticipants)
+{
+  ApHandleTreeIterator<int> iter(removedParticipants);
+  for (ApHandleTreeNode<int>* pNode = 0; (pNode = iter.Next()) != 0; ) {
+    ApHandle hParticipant = pNode->Key();
+    Avatar* pAvatar = 0;
+    avatars_.Get(hParticipant, pAvatar);
+    if (pAvatar) {
+      pAvatar->Destroy();
+      avatars_.Unset(hParticipant);
+      delete pAvatar;
+      pAvatar = 0;
+    }
+  }
+}
+
+//---------------------------------------------------
+
+void Display::RemoveAllAvatars()
+{
+  ParticipantFlags removedParticipants;
+
+  InitRemovedParticipants(removedParticipants);
+  ProcessRemovedParticipants(removedParticipants);
+}
+
+void Display::RemoveAllObjects()
+{
+}
+
+void Display::ResetLocationInfo()
+{
+}
+
+//---------------------------------------------------
+
+void Display::OnContextDetailsChanged(Apollo::ValueList& vlKeys)
+{
+  Msg_VpView_GetContextDetail msg;
+  msg.hContext = hContext_;
+
+  for (Apollo::ValueElem* e = 0; e = vlKeys.nextElem(e); ) {
+    msg.sKey = e->getString();
+
+    if (0) {
+    } else if (e->getString() == Msg_VpView_ContextDetail_DocumentUrl) {
+      if (msg.Request()) {
+        DisplaySrpcMessage dsm(this, "SetDocumentUrl");
+        dsm.srpc.setString("sUrl", msg.sValue);
+        dsm.Request();
+      }
+    } else if (e->getString() == Msg_VpView_ContextDetail_LocationUrl) {
+      if (msg.Request()) {
+        DisplaySrpcMessage dsm(this, "SetLocationUrl");
+        dsm.srpc.setString("sUrl", msg.sValue);
+        dsm.Request();
+      }
+    }
+  }
+}
+
+//---------------------------------------------------
+
+DisplaySrpcMessage::DisplaySrpcMessage(Display* pDisplay, const String& sMethod)
+{
+  srpc.setString("Method", sMethod);
+  hView = pDisplay->GetView();
+  sFunction = Apollo::getModuleConfig(MODULE_NAME, "CallScriptSrpcFunctionName", "receiveSrpcMessageAsString");
+}
+
---
+++ trunk/Apollo/src/Arena/WebArenaModule.cpp
@@ -0,0 +1,453 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#include "Apollo.h"
+#include "MsgConfig.h"
+#include "Local.h"
+#include "WebArenaModule.h"
+#include "Avatar.h"
+
+Display* WebArenaModule::CreateDisplay(const ApHandle& hContext)
+{
+  Display* pDisplay = new Display(this, hContext);
+  if (pDisplay) {
+    int ok = pDisplay->Create();
+    if (ok ) {
+      displays_.Set(hContext, pDisplay);
+    } else {
+      delete pDisplay;
+      pDisplay = 0;
+    }
+  }
+  return pDisplay;
+}
+
+void WebArenaModule::DeleteDisplay(const ApHandle& hContext)
+{
+  Display* pDisplay = FindDisplay(hContext);
+  if (pDisplay) {
+    pDisplay->Destroy();
+    displays_.Unset(hContext);
+    delete pDisplay;
+    pDisplay = 0;
+  }
+}
+
+Display* WebArenaModule::FindDisplay(const ApHandle& hContext)
+{
+  Display* pDisplay = 0;
+  displays_.Get(hContext, pDisplay);
+  return pDisplay;
+}
+
+//---------------------------
+
+//void WebArenaModule::DeleteOldLeaveRequestedLocations()
+//{
+//  int bDone = 0;
+//  while (!bDone) {
+//    ApHandle hLocation;
+//    bDone = 1;
+//
+//    LocationListIterator iter(locations_);
+//    for (LocationListNode* pNode = 0; (pNode = iter.Next()) != 0; ) {
+//      Location* pLocation = pNode->Value();
+//      if (pLocation) {
+//        int bTooOld = pLocation->TellDeleteMe();
+//        if (bTooOld) {
+//          hLocation = pNode->Key();
+//          bDone = 0;
+//          break;
+//        }
+//      }
+//    }
+//
+//    if (ApIsHandle(hLocation)) {
+//      apLog_Info((LOG_CHANNEL, "WebArenaModule::DeleteOldLeaveRequestedLocations", "deleting location=" ApHandleFormat "", ApHandleType(hLocation)));
+//      DeleteLocation(hLocation);
+//    }
+//  }
+//}
+
+//---------------------------
+
+void WebArenaModule::SetContextOfHandle(const ApHandle& h, const ApHandle& hContext)
+{
+  if (contextOfHandle_.IsSet(h)) {
+    contextOfHandle_.Unset(h);
+  }
+  contextOfHandle_.Set(h, hContext);
+}
+
+void WebArenaModule::DeleteContextOfHandle(const ApHandle& h, const ApHandle& hContext)
+{
+  ApHandle hDelete;
+  contextOfHandle_.Get(h, hDelete);
+  if (hDelete != hContext) {
+    apLog_Warning((LOG_CHANNEL, "WebArenaModule::DeleteContextOfHandle", "Context not found for handle=" ApHandleFormat " ctxt=" ApHandleFormat "", ApHandleType(h), ApHandleType(hContext)));
+  } else {
+    contextOfHandle_.Unset(h);
+  }
+}
+
+ApHandle WebArenaModule::GetContextOfHandle(const ApHandle& h)
+{
+  if (contextOfHandle_.IsSet(h)) {
+    return contextOfHandle_.Find(h)->Value();
+  }
+  return ApNoHandle;
+}
+
+Display* WebArenaModule::GetDisplayOfHandle(const ApHandle& h)
+{
+  ApHandle hContext = GetContextOfHandle(h);
+  if (ApIsHandle(hContext)) {
+    return FindDisplay(hContext);
+  }
+  return 0;
+}
+
+//---------------------------
+
+void WebArenaModule::SetParticipantOfAnimation(const ApHandle& hAnimation, const ApHandle& hParticipant)
+{
+  if (participantOfAnimation_.IsSet(hAnimation)) {
+    participantOfAnimation_.Unset(hAnimation);
+  }
+  participantOfAnimation_.Set(hAnimation, hParticipant);
+}
+
+void WebArenaModule::DeleteParticipantOfAnimation(const ApHandle& hAnimation, const ApHandle& hParticipant)
+{
+  ApHandle h;
+  participantOfAnimation_.Get(hAnimation, h);
+  if (h != hParticipant) {
+    apLog_Warning((LOG_CHANNEL, "WebArenaModule::DeleteParticipantOfAnimation", "Participant not found for loc=" ApHandleFormat " ctxt=" ApHandleFormat "", ApHandleType(hParticipant)));
+  } else {
+    participantOfAnimation_.Unset(hAnimation);
+  }
+}
+
+ApHandle WebArenaModule::GetParticipantOfAnimation(const ApHandle& hAnimation)
+{
+  if (participantOfAnimation_.IsSet(hAnimation)) {
+    return participantOfAnimation_.Find(hAnimation)->Value();
+  }
+  return ApNoHandle;
+}
+
+//----------------------------------------------------------
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextCreated)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    apLog_Warning((LOG_CHANNEL, "WebArenaModule:VpView_ContextCreated", "Display already exists for ctxt=" ApHandleFormat "", ApHandleType(pMsg->hContext)));
+  } else {
+    pDisplay = CreateDisplay(pMsg->hContext);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextDestroyed)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay == 0) {
+    apLog_Warning((LOG_CHANNEL, "WebArenaModule::VpView_ContextDestroyed", "No display for ctxt=" ApHandleFormat "", ApHandleType(pMsg->hContext)));
+  } else {
+    DeleteDisplay(pMsg->hContext);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextVisibility)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->SetVisibility(pMsg->bVisible);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextPosition)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->SetPosition(pMsg->nX, pMsg->nY);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextSize)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->SetSize(pMsg->nWidth, pMsg->nHeight);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LocationsChanged){}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextLocationAssigned)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->AttachLocation(pMsg->hLocation);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextLocationUnassigned)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->DetachLocation(pMsg->hLocation);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_EnterLocationRequested)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnEnterRequested();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_EnterLocationBegin)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnEnterBegin();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_EnterLocationComplete)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnEnterComplete();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LocationContextsChanged) {}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ParticipantsChanged)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnParticipantsChanged();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LocationPublicChat)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    Apollo::TimeValue tv(pMsg->nSec, pMsg->nMicroSec);
+    pDisplay->OnReceivePublicChat(pMsg->hParticipant, pMsg->hChat, pMsg->sNickname, pMsg->sText, tv);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LocationDetailsChanged) {}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ContextDetailsChanged)
+{
+  Display* pDisplay = FindDisplay(pMsg->hContext);
+  if (pDisplay) {
+    pDisplay->OnContextDetailsChanged(pMsg->vlKeys);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ParticipantDetailsChanged)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnParticipantDetailsChanged(pMsg->hParticipant, pMsg->vlKeys);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LeaveLocationRequested)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnLeaveRequested();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LeaveLocationBegin)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnLeaveBegin();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_LeaveLocationComplete)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hLocation);
+  if (pDisplay) {
+    pDisplay->OnLeaveComplete();
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ParticipantAdded){}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, VpView_ParticipantRemoved){}
+
+//----------------------------
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, Animation_SequenceBegin)
+{
+  Display* pDisplay = GetDisplayOfHandle(pMsg->hItem);
+  if (pDisplay) {
+    ApHandle hParticipant = GetParticipantOfAnimation(pMsg->hItem);
+    String sUrl = pMsg->sUrl;
+    if (!sUrl) {
+      sUrl = pMsg->sSrc;
+    }
+    pDisplay->OnAvatarAnimationBegin(hParticipant, sUrl);
+  }
+}
+
+//AP_MSG_HANDLER_METHOD(WebArenaModule, Animation_Frame)
+//{
+//  Display* pDisplay = GetDisplayOfHandle(pMsg->hItem);
+//  if (pDisplay) {
+//    ApHandle hParticipant = GetParticipantOfAnimation(pMsg->hItem);
+//    pDisplay->OnAvatarAnimationFrame(hParticipant, pMsg->iFrame);
+//  }
+//}
+//
+//AP_MSG_HANDLER_METHOD(WebArenaModule, Animation_SequenceEnd)
+//{
+//  Display* pDisplay = GetDisplayOfHandle(pMsg->hItem);
+//  if (pDisplay) {
+//    ApHandle hParticipant = GetParticipantOfAnimation(pMsg->hItem);
+//    pDisplay->OnAvatarAnimationEnd(hParticipant);
+//  }
+//}
+
+//----------------------------
+
+AP_SRPC_HANDLER_METHOD(WebArenaModule, WebArena_CallModuleSrpc, ApSRPCMessage)
+{
+  String sView = pMsg->srpc.getString("hView");
+  if (!sView) { throw ApException("Missing hView"); }
+
+  ApHandle hView = Apollo::string2Handle(sView);
+  if (!ApIsHandle(hView)) { throw ApException("Not a handle: %s", StringType(sView)); }
+
+  Display* pDisplay = GetDisplayOfHandle(hView);
+  if (pDisplay) {
+    pDisplay->OnCallModuleSrpc(pMsg->srpc, pMsg->response);
+  }
+  pMsg->apStatus = ApMessage::Ok;
+}
+
+//----------------------------------------------------------
+
+#if defined(AP_TEST)
+
+ApHandle Test_Avatar_RemoveOldChats_hRemovedChat_;
+
+void Test_Avatar_RemoveOldChats_DisplaySrpcMessage(DisplaySrpcMessage* pMsg)
+{
+  if (pMsg->srpc.getString("Method") == "RemoveAvatarChat") {
+    Test_Avatar_RemoveOldChats_hRemovedChat_ = Apollo::string2Handle(pMsg->srpc.getString("hChat"));
+  }
+}
+
+static String Test_Avatar_RemoveOldChats()
+{
+  String s;
+
+  WebArenaModule m;
+  Display d(&m, Apollo::newHandle());
+  Avatar a(&m, &d, Apollo::newHandle());
+
+  { DisplaySrpcMessage msg(&d, "Dummy"); msg.Hook(MODULE_NAME, (ApCallback) Test_Avatar_RemoveOldChats_DisplaySrpcMessage, 0, ApCallbackPosEarly); }  
+
+  ApHandle hChat2 = Apollo::newHandle();
+  ApHandle hChat1 = Apollo::newHandle();
+  ApHandle hChat4 = Apollo::newHandle();
+  ApHandle hChat3 = Apollo::newHandle();
+
+  a.OnReceivePublicChat(hChat4, "Nickname4", "Text4", Apollo::TimeValue(4, 0));
+  a.OnReceivePublicChat(hChat3, "Nickname3", "Text3", Apollo::TimeValue(3, 0));
+  a.OnReceivePublicChat(hChat1, "Nickname1", "Text1", Apollo::TimeValue(1, 0));
+  a.OnReceivePublicChat(hChat2, "Nickname2", "Text2", Apollo::TimeValue(2, 0));
+
+  if (Test_Avatar_RemoveOldChats_hRemovedChat_ != hChat1) {
+    s = "Did not remove oldest chat";
+  }
+
+  { DisplaySrpcMessage msg(&d, "Dummy"); msg.UnHook(MODULE_NAME, (ApCallback) Test_Avatar_RemoveOldChats_DisplaySrpcMessage, 0); }  
+
+  return s;
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, UnitTest_Begin)
+{
+  AP_UNUSED_ARG(pMsg);
+  if (Apollo::getConfig("Test/Arena", 0)) {
+    AP_UNITTEST_REGISTER(Test_Avatar_RemoveOldChats);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, UnitTest_Execute)
+{
+  AP_UNUSED_ARG(pMsg);
+  if (Apollo::getConfig("Test/Arena", 0)) {
+    AP_UNITTEST_EXECUTE(Test_Avatar_RemoveOldChats);
+  }
+}
+
+AP_MSG_HANDLER_METHOD(WebArenaModule, UnitTest_End)
+{
+  AP_UNUSED_ARG(pMsg);
+}
+
+#endif // #if defined(AP_TEST)
+
+//----------------------------------------------------------
+
+int WebArenaModule::Init()
+{
+  int ok = 1;
+
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextCreated, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextDestroyed, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextVisibility, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextPosition, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextSize, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LocationsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextLocationAssigned, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextLocationUnassigned, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_EnterLocationRequested, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_EnterLocationBegin, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_EnterLocationComplete, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LocationContextsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ParticipantsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LocationPublicChat, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LocationDetailsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ContextDetailsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ParticipantDetailsChanged, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LeaveLocationRequested, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LeaveLocationBegin, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_LeaveLocationComplete, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ParticipantAdded, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, VpView_ParticipantRemoved, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, Animation_SequenceBegin, this, ApCallbackPosNormal);
+  //AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, Animation_Frame, this, ApCallbackPosNormal);
+  //AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, Animation_SequenceEnd, this, ApCallbackPosNormal);
+  AP_MSG_REGISTRY_ADD(MODULE_NAME, WebArenaModule, WebArena_CallModuleSrpc, this, ApCallbackPosNormal);
+
+  AP_UNITTEST_HOOK(WebArenaModule, this);
+
+  return ok;
+}
+
+void WebArenaModule::Exit()
+{
+  AP_UNITTEST_UNHOOK(WebArenaModule, this);
+
+  AP_MSG_REGISTRY_FINISH;
+}
