659f8324f541b08bbb49df1cc490374e690bac45
Heiner Wolf <wolfspelz@users.noreply.github.com>
2011-06-12 00:38:30 +0000

    ChatWindow

---
+++ Apollo/src/Chat/Local.h
@@ -0,0 +1,20 @@
+// ============================================================================
+//
+// Apollo
+//
+// ============================================================================
+
+#if !defined(Local_H_INCLUDED)
+#define Local_H_INCLUDED
+
+#define MODULE_NAME "ChatWindow"
+#define LOG_CHANNEL MODULE_NAME
+
+#include "ApLog.h"
+#include "MsgChatWindow.h"
+#include "MsgVp.h"
+#include "MsgWebView.h"
+#include "MsgDialog.h"
+#include "ChatModule.h"
+
+#endif // Local_H_INCLUDED
