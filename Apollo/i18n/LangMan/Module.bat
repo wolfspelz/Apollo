070650742564ce8d64309338d399acd073ab1194
Heiner Wolf <wolfspelz@users.noreply.github.com>
2011-06-18 11:31:43 +0000

    Cleaning up for ReleaseBuilder

--- Apollo/i18n/LangMan/Module.bat
+++ Apollo/i18n/LangMan/Module.bat
@@ -1,5 +1,10 @@
+@echo off
 REM Run LangMan for a module. Then copy files Win32/Debug and Win32/Release folders
+@echo on
 
 java -jar LangMan.jar -v -p %1 -l en -l de
 xcopy /F /Y out\%1\*.txt ..\..\bin\Win32\Debug\modules\%1\
-xcopy /F /Y out\%1\*.txt ..\..\bin\Win32\Release\modules\%1\
+
+@echo off
+REM Release files will be copied by BuildPackage.bat
+REM xcopy /F /Y out\%1\*.txt ..\..\bin\Win32\Release\modules\%1\
