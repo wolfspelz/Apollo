e787d848f0502c888759829edc10c808f1030727
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2011-06-18 02:16:37 +0000

    LangMan: automate modules

--- trunk/Apollo/i18n/LangMan/Generate_Modules.bat
+++ trunk/Apollo/i18n/LangMan/Generate_Modules.bat
@@ -1,9 +1,2 @@
-java -jar LangMan.jar -v -p Arena -l en -l de
-xcopy /F /Y out\Arena\*.txt ..\..\bin\Win32\Debug\modules\Arena\
-xcopy /F /Y out\Arena\*.txt ..\..\bin\Win32\Release\modules\Arena\
-
-java -jar LangMan.jar -v -p Chat -l en -l de
-xcopy /F /Y out\Arena\*.txt ..\..\bin\Win32\Debug\modules\Chat\
-xcopy /F /Y out\Arena\*.txt ..\..\bin\Win32\Release\modules\Chat\
-
-pause
+DEL Generate_Modules.log
+FOR %%i IN (Arena,Chat) DO Module.bat %%i >> Generate_Modules.log
