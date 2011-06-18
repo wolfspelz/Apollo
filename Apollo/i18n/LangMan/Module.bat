ea6a636c911452b2730f0c5da2e8278b0497d633
Heiner Wolf <wolfspelz@users.noreply.github.com>
2011-06-18 02:19:18 +0000

    LangMan: automate modules

--- Apollo/i18n/LangMan/Module.bat
+++ Apollo/i18n/LangMan/Module.bat
@@ -1,3 +1,5 @@
+REM Run LangMan for a module. Then copy files Win32/Debug and Win32/Release folders
+
 java -jar LangMan.jar -v -p %1 -l en -l de
 xcopy /F /Y out\%1\*.txt ..\..\bin\Win32\Debug\modules\%1\
 xcopy /F /Y out\%1\*.txt ..\..\bin\Win32\Release\modules\%1\
