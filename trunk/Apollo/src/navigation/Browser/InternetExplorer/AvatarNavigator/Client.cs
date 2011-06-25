16f9ce091445737ba775564777f8103c5a889731
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2011-06-25 13:49:33 +0000

    IE BHO: research installer

--- trunk/Apollo/src/navigation/Browser/InternetExplorer/AvatarNavigator/Client.cs
+++ trunk/Apollo/src/navigation/Browser/InternetExplorer/AvatarNavigator/Client.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
-namespace SpicIE.AvatarNavigator
+namespace OVW
 {
     class Client
     {
--- trunk/Apollo/src/navigation/Browser/InternetExplorer/AvatarNavigator/Srpc.cs
+++ trunk/Apollo/src/navigation/Browser/InternetExplorer/AvatarNavigator/Srpc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 
-namespace SpicIE.AvatarNavigator
+namespace OVW
 {
     public class Srpc
     {
