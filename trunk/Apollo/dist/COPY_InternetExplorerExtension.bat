c92c21f1f92ae8872d0a78231c9a67c8fda7e268
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2011-07-16 15:42:38 +0000

    COPY_InternetExplorerExtension.bat

---
+++ trunk/Apollo/dist/COPY_InternetExplorerExtension.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+PATH=%PATH%;%CD%\bin
+
+set PRODUCT_NAME=AvatarNavigator
+set DEST_FILE=%PRODUCT_NAME%.msi
+
+set WORK_DIR=%CD%
+set BASE_DIR=%WORK_DIR%\..
+set SRC_DIR=%BASE_DIR%\src\navigation\Browser\InternetExplorer\Setup\Release
+set DEST_DIR=%BASE_DIR%\bin\Win32\Debug\modules\navigation
+
+echo ---------------------------------------------------------------------------
+echo Copying result to Debug destination
+xcopy %SRC_DIR%\%DEST_FILE% %DEST_DIR%\ /R /Y
