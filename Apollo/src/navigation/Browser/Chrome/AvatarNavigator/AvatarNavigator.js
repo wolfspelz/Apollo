31b9979c1692f2593646254a6fb79ad213dd2e11
Heiner Wolf <wolfspelz@users.noreply.github.com>
2011-08-10 14:36:30 +0000

    Chrome ext

--- Apollo/src/navigation/Browser/Chrome/AvatarNavigator/AvatarNavigator.js
+++ Apollo/src/navigation/Browser/Chrome/AvatarNavigator/AvatarNavigator.js
@@ -1,34 +1,40 @@
 var DEFAULT_HOSTNAME = 'localhost';
-var DEFAULT_PORT = 23763;
-var MIN_RECONNECT_DELAY = 1000;
+var DEFAULT_PORT = 23765;
+var MIN_RECONNECT_DELAY = 100;
 var MAX_RECONNECT_DELAY = 10000;
 var TYPE_CHROME = 'Chrome';
 
-if (!window.console) console = {};
-console.log = console.log || function () {};
-console.warn = console.warn || function () {};
-console.error = console.error || function () {};
-console.info = console.info || function () {};
+var sServerHostname = DEFAULT_HOSTNAME;
+var nServerPort = DEFAULT_PORT + 1;
+var nReconnectDelay = MIN_RECONNECT_DELAY;
+var webSocket = null;
+var srpcProtocol = null;
 
-var aWindows = new Object();
-var aTabs = new Object();
+var aWindows = null;
+var aTabs = null;
 
-function Window(windowId)
+var anLogLevel = anLogLevelDebug;
+
+// --------------------------
+// Window
+
+function Window(windowId, window)
 {
   this.id = windowId;
-  this.selectedTabId = '';
+  this.selectedTabId = null;
+  this.window = window;
 }
 
-function AddWindow(windowId)
+function AddWindow(windowId, window)
 {
-  console.log('AddWindow' + windowId);
-  aWindows[windowId] = new Window(windowId);
+  anLogVerbose('AddWindow' + windowId);
+  aWindows[windowId] = new Window(windowId, window);
 }
 
 function DeleteWindow(windowId)
 {
   if (aWindows[windowId] != null) {
-    console.log('DeleteWindow' + windowId);
+    anLogVerbose('DeleteWindow' + windowId);
     delete aWindows[windowId];
   }
 }
@@ -40,34 +46,39 @@ function SetWindowSelectedTab(windowId, tabId)
 {
   var myWindow = GetWindow(windowId);
   if (myWindow != null) {
-    var currentTab = myWindow.selectedTab;
-    if (currentTab != tabId) {
-      if (currentTab != '') {
-        OnTabHide(currentTab);
-      }
-      OnTabShow(tabId);
-      myWindow.selectedTab = tabId;
+    var currentTabId = myWindow.selectedTabId;
+    if (currentTabId != null) {
+      OnHideTab(currentTabId);
     }
+    OnShowTab(tabId);
+    myWindow.selectedTabId = tabId;
   }
 }
 
-function Tab(tabId)
+// --------------------------
+// Tab
+
+function Tab(tabId, windowId, tab)
 {
   this.id = tabId;
-  this.url = '';
-  this.context = '';
+  this.windowId = windowId;
+  this.sUrl = '';
+  this.sContext = '';
+  this.bVisible = false;
+  this.tab = tab;
 }
 
-function AddTab(tabId)
+function AddTab(tabId, windowId, tab)
 {
-  console.log('AddTab ' + tabId);
-  aTabs[tabId] = new Tab(tabId);
+  anLogVerbose('AddTab ' + tabId);
+  aTabs[tabId] = new Tab(tabId, windowId, tab);
 }
 
 function DeleteTab(tabId)
 {
+  OnCloseTab(tabId);
   if (aTabs[tabId] != null) {
-    console.log('DeleteTab ' + tabId);
+    anLogVerbose('DeleteTab ' + tabId);
     delete aTabs[tabId];
   }
 }
@@ -75,41 +86,235 @@ function DeleteTab(tabId)
 function GetTab(tabId) { return aTabs[tabId]; }
 function HasTab(tabId) { return aTabs[tabId] != null; }
 
-function SetTabUrl(tabId, sUrl)
+// --------------------------
+// hContext
+
+function RequestTabContext(tabId)
 {
+  var fCallback = function (response)
+  {
+    var myTab = GetTab(tabId);
+    if (myTab != null) {
+      if (response.getInt('Status') == 1) {
+        var hContext = response.getString('hResult');
+        if (hContext != '') {
+          anLogInfo('TabContext tab=' + tabId + ' ctxt=' + hContext);
+          myTab.sContext = hContext;
+
+          OpenContext(myTab.sContext);
+
+          IdentifyContext(myTab.sContext, myTab.windowId);
+
+          if (myTab.bVisible) {
+            ShowContext(myTab.sContext);
+          } else {
+            HideContext(myTab.sContext);
+          }
+
+          NavigateContext(myTab.sContext, myTab.sUrl);
+        }
+      }
+    }
+  }
+
   var myTab = GetTab(tabId);
   if (myTab != null) {
-    var sOldUrl = myTab.url;
-    if (sOldUrl != sUrl) {
-      myTab.url = sUrl;
-      OnTabUrlChanged(tabId);
+    if (srpcProtocol != null) {
+      srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'System_GetHandle'), fCallback);
+    } else {
+      anLogWarning('RequestTabContext ignored(not-connected)');
     }
   }
 }
 
-function OnTabShow(tabId)
+// --------------------------
+// Core tab events
+
+function OnOpenTab(tabId)
 {
+  anLogInfo('OnOpenTab ' + tabId);
   var myTab = GetTab(tabId);
   if (myTab != null) {
-    console.log('OnTabShow ' + tabId);
+    OpenContext(myTab.sContext);
   }
 }
 
-function OnTabHide(tabId)
+function OnShowTab(tabId)
 {
+  var bVisible = true;
+  anLogInfo('OnShowTab ' + tabId);
   var myTab = GetTab(tabId);
   if (myTab != null) {
-    console.log('OnTabHide ' + tabId);
+    var bOldVisible = myTab.bVisible;
+    if (bOldVisible != bVisible) {
+      myTab.bVisible = bVisible;
+      if (myTab.sContext != '') {
+        ShowContext(myTab.sContext);
+      }
+    }
   }
 }
 
-function OnTabUrlChanged(tabId)
+function OnHideTab(tabId)
 {
+  var bVisible = false;
+  anLogInfo('OnHideTab ' + tabId);
   var myTab = GetTab(tabId);
   if (myTab != null) {
-    console.log('OnTabUrlChanged ' + tabId + ' ctxt=' + myTab.context + ' url=' + myTab.url)
+    var bOldVisible = myTab.bVisible;
+    if (bOldVisible != bVisible) {
+      myTab.bVisible = bVisible;
+      if (myTab.sContext != '') {
+        HideContext(myTab.sContext);
+      }
+    }
   }
 }
+
+function OnNavigateTab(tabId, sUrl)
+{
+  anLogInfo('OnNavigateTab ' + tabId + ' ' + sUrl);
+  var myTab = GetTab(tabId);
+  if (myTab != null) {
+    var sOldUrl = myTab.sUrl;
+    if (sOldUrl != sUrl) {
+      myTab.sUrl = sUrl;
+      if (myTab.sContext != '') {
+        NavigateContext(myTab.sContext, sUrl);
+      }
+    }
+  }
+}
+
+function OnReparentTab(tabId, newWindowId)
+{
+  anLogInfo('OnReparentTab tab=' + tabId + ' to win=' + newWindowId);
+  var myTab = GetTab(tabId);
+  if (myTab != null) {
+    if (myTab.windowId != newWindowId) {
+      myTab.windowId = newWindowId;
+      IdentifyContext(myTab.sContext, newWindowId);
+    }
+  }
+}
+
+function OnCloseTab(tabId)
+{
+  anLogInfo('OnCloseTab ' + tabId);
+  var myTab = GetTab(tabId);
+  if (myTab != null) {
+    if (myTab.sContext != '') {
+      CloseContext(myTab.sContext);
+    }
+  }
+}
+
+// --------------------------
+// Context
+
+function OpenContext(sContext)
+{
+  anLogVerbose('OpenContext ' + sContext);
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_ContextOpen').setString('hContext', sContext));
+  } else {
+    anLogWarning('OpenContext ignored(not-connected) ' + sContext);
+  }
+}
+
+function IdentifyContext(sContext, windowId)
+{
+  anLogVerbose('IdentifyContext' + sContext);
+  var myWindow = GetWindow(windowId);
+  if (myWindow != null) {
+    if (srpcProtocol != null) {
+
+      var msg = new SrpcMessage();
+      msg.setString('Method', 'Navigation_ContextNativeWindow');
+      msg.setString('hContext', sContext);
+
+      var selectedTab = null;
+      var aTabs = myWindow.window.tabs;
+      if (aTabs != null) {
+        for (var i = 0; i < aTabs.length; ++i) {
+          var tab = aTabs[i];
+          if (tab.selected) {
+            selectedTab = tab;
+          }
+        }
+      }
+      var sTitle = '';
+      if (selectedTab != null) {
+        sTitle = selectedTab.title;
+      }
+
+      var kvSignature = new SrpcMessage();
+      kvSignature.setString('sType', TYPE_CHROME);
+      kvSignature.setString('sVersion', navigator.userAgent);
+      if (sTitle != '') {
+        kvSignature.setString('sTitle', sTitle);
+      }
+      kvSignature.setString('nLeft', myWindow.window.left);
+      kvSignature.setString('nTop', myWindow.window.top);
+      kvSignature.setString('nWidth', myWindow.window.width);
+      kvSignature.setString('nHeight', myWindow.window.height);
+      msg.setString('kvSignature', kvSignature.toString());
+
+      srpcProtocol.sendRequest(msg);
+
+    } else {
+      anLogWarning('IdentifyContext ignored(not-connected) ' + sContext);
+    }
+  }
+}
+
+function ShowContext(sContext)
+{
+  anLogVerbose('ShowContext ' + sContext);
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_ContextShow').setString('hContext', sContext));
+  } else {
+    anLogWarning('ShowContext ignored(not-connected) ' + sContext);
+  }
+}
+
+function HideContext(sContext)
+{
+  anLogVerbose('HideContext ' + sContext);
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_ContextHide').setString('hContext', sContext));
+  } else {
+    anLogWarning('HideContext ignored(not-connected) ' + sContext);
+  }
+}
+
+function NavigateContext(sContext, sUrl)
+{
+  if (sUrl.indexOf('http://') == 0 || sUrl.indexOf('https://') == 0) {
+  } else {
+    sUrl = '';
+  }
+
+  anLogVerbose('NavigateContext ' + sContext + ' ' + sUrl)
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_ContextNavigate').setString('hContext', sContext).setString('sUrl', sUrl));
+  } else {
+    anLogWarning('NavigateContext ignored(not-connected) ' + sContext);
+  }
+}
+
+function CloseContext(sContext)
+{
+  anLogVerbose('CloseContext ' + sContext)
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_ContextClose').setString('hContext', sContext));
+  } else {
+    anLogWarning('CloseContext ignored(not-connected) ' + sContext);
+  }
+}
+
+// --------------------------
+// chrome.tab and crome.window hooks
 
 function OnWindowGetSelectedTab(tab)
 {
@@ -126,6 +331,12 @@ function OnWindowGetAllResult(windows)
     for (var j = 0; j < nTabs; j++) {
       var tab = win.tabs[j];
       OnTabCreated(tab);
+      OnNavigateTab(tab.id, tab.url);
+      if (tab.selected) {
+        OnShowTab(tab.id);
+      } else {
+        OnHideTab(tab.id);
+      }
     }
     chrome.tabs.getSelected(win.id, OnWindowGetSelectedTab);
   }
@@ -134,101 +345,167 @@ function OnWindowGetAllResult(windows)
 function OnWindowCreated(win)
 {
   var windowId = win.id;
-  //console.log('OnWindowCreated ' + 'left=' + win.left + ' top=' + win.top + ' width=' + win.width + ' height=' + win.height + ' ');
+  //anLogVerbose('OnWindowCreated ' + 'left=' + win.left + ' top=' + win.top + ' width=' + win.width + ' height=' + win.height + ' ');
   if (win.type == 'normal') {
-    AddWindow(windowId);
+    AddWindow(windowId, win);
   }
 }
 
 function OnWindowRemoved(windowId)
 {
-  //console.log('OnWindowRemoved ' + windowId);
+  //anLogVerbose('OnWindowRemoved ' + windowId);
   DeleteWindow(windowId);
 }
 
 function OnTabCreated(tab)
 {
-  //console.log('OnTabCreated');
-  ProcessTab(tab);
+  //anLogVerbose('OnTabCreated');
+  var tabId = tab.id;
+  if (!HasTab(tabId)) {
+    AddTab(tabId, tab.windowId, tab);
+    RequestTabContext(tabId);
+  }
 }
 
 function OnTabRemoved(tabId, removeInfo)
 {
-  //console.log('OnTabRemoved ' + tabId);
+  //anLogVerbose('OnTabRemoved ' + tabId);
   DeleteTab(tabId);
 }
 
 function OnTabUpdated(tabId, changeInfo, tab)
 {
-  //console.log('OnTabUpdated');
-  ProcessTab(tab);
+  //anLogVerbose('OnTabUpdated');
+  if (HasTab(tabId) && tab.url != null) {
+    OnNavigateTab(tabId, tab.url);
+  }
 }
 
 function OnTabSelectionChanged(tabId, selectInfo)
 {
-  //console.log('OnTabSelectionChanged');
+  //anLogVerbose('OnTabSelectionChanged');
   SetWindowSelectedTab(selectInfo.windowId, tabId);
-}
-
-function OnTabAttached(tabId, attachInfo)
-{
-  console.log('OnTabAttached');
 }
 
 function OnTabDetached(tabId, detachInfo)
 {
-  console.log('OnTabDetached');
-}
-
-function ProcessTab(tab)
-{
-  var tabId = tab.id;
-
-  if (HasTab(tabId)) {
-    //console.log('ProcessTab HasTab ' + GetTab(tabId).id);
-  } else {
-    AddTab(tabId);
-  }
-
-  if (HasTab(tabId)) {
-    RequestTabContext(tabId);
-  }
-
-  if (HasTab(tabId) && tab.url != null) {
-    var sUrl = tab.url;
-    if (sUrl.indexOf('http://') === 0 || sUrl.indexOf('https://') === 0) {
-      SetTabUrl(tabId, sUrl);
-    }
-  }
-}
-
-function RequestTabContext(tabId)
-{
+  anLogVerbose('OnTabDetached');
   var myTab = GetTab(tabId);
   if (myTab != null) {
-    
+//     if (myTab.bVisible) {
+//       myTab.bAttachVisible = true;
+//       OnHideTab(tabId);
+//     }
   }
+}
+
+function OnTabAttached(tabId, attachInfo)
+{
+  anLogVerbose('OnTabAttached');
+  var myTab = GetTab(tabId);
+  if (myTab != null) {
+    OnReparentTab(tabId, attachInfo.newWindowId);
+//     if (myTab.bAttachVisible) {
+//       myTab.bAttachVisible = false;
+//       OnShowTab(tabId);
+//     }
+  }
+}
+
+// --------------------------
+// Start
+
+function StartTabs()
+{
+  aWindows = new Object();
+  aTabs = new Object();
+
+  chrome.windows.getAll({ 'populate': true }, OnWindowGetAllResult);
+
+  chrome.windows.onCreated.addListener(OnWindowCreated);
+  chrome.windows.onRemoved.addListener(OnWindowRemoved);
+  chrome.tabs.onCreated.addListener(OnTabCreated);
+  chrome.tabs.onRemoved.addListener(OnTabRemoved);
+  chrome.tabs.onUpdated.addListener(OnTabUpdated);
+  chrome.tabs.onSelectionChanged.addListener(OnTabSelectionChanged);
+  chrome.tabs.onAttached.addListener(OnTabAttached);
+  chrome.tabs.onDetached.addListener(OnTabDetached);
 }
 
 function Start()
 {
-//  chrome.windows.getAll({ 'populate': true }, OnWindowGetAllResult);
-
-//  chrome.windows.onCreated.addListener(OnWindowCreated);
-//  chrome.windows.onRemoved.addListener(OnWindowRemoved);
-//  chrome.tabs.onCreated.addListener(OnTabCreated);
-//  chrome.tabs.onRemoved.addListener(OnTabRemoved);
-//  chrome.tabs.onUpdated.addListener(OnTabUpdated);
-//  chrome.tabs.onSelectionChanged.addListener(OnTabSelectionChanged);
-//  chrome.tabs.onAttached.addListener(OnTabAttached);
-//  chrome.tabs.onDetached.addListener(OnTabDetached);
-
-  var myWebSocket = new WebSocket("ws://localhost:23766");
-  myWebSocket.onopen = function (evt) { myWebSocket.send("Method=System_GetHandle"); };
-  myWebSocket.onmessage = function (evt) { alert("" + evt.data); };
-  myWebSocket.onclose = function (evt) { alert("Connection closed."); };
+  StartReconnectTimer();
 }
 
-// http://datatracker.ietf.org/doc/draft-ietf-hybi-thewebsocketprotocol/?include_text=1
+function StartReconnectTimer()
+{
+  nReconnectDelay *= 2;
+  if (nReconnectDelay > MAX_RECONNECT_DELAY) {
+    nReconnectDelay = MAX_RECONNECT_DELAY;
+  }
+  anLogInfo('StartReconnectTimer: scheduling connect in  ' + nReconnectDelay + ' msec');
+  window.setTimeout('Reconnect();', nReconnectDelay);
+}
+
+// --------------------------
+// Connection
+
+function OnReceiveMessage(msg)
+{
+  var sMethod = msg.getString('Method');
+  anLogInfo('OnReceiveMessage Method=' + sMethod);
+}
+
+function OnSendData(sData)
+{
+  anLogInfo('OUT: ' + sData);
+  webSocket.send(sData);
+}
+
+function OnHelloResponse(msg)
+{
+  StartTabs();
+}
+
+function OnWebSocketOpened(evt)
+{
+  anLogInfo('OnWebSocketOpened');
+  nReconnectDelay = MIN_RECONNECT_DELAY;
+  
+  srpcProtocol = new SrpcProtocol();
+  srpcProtocol.onReceiveMessage = OnReceiveMessage;
+  srpcProtocol.onSendData = OnSendData;
+
+  if (srpcProtocol != null) {
+    srpcProtocol.sendRequest(new SrpcMessage().setString('Method', 'Navigation_NavigatorHello'), OnHelloResponse);
+  }
+}
+
+function OnWebSocketMesssge(evt)
+{
+  var sData  = evt.data;
+  anLogInfo('IN: ' + sData);
+  srpcProtocol.handleMessage(sData);
+}
+
+function OnWebSocketClosed(evt)
+{
+  anLogInfo('OnWebSocketClosed');
+  webSocket = null;
+  srpcProtocol = null;
+  StartReconnectTimer();
+}
+
+function Reconnect()
+{
+  var sUrl = 'ws://' + sServerHostname + ':' + nServerPort;
+  anLogInfo('Connecting to ' + sUrl);
+  webSocket = new WebSocket(sUrl);
+  webSocket.onopen = OnWebSocketOpened;
+  webSocket.onmessage = OnWebSocketMesssge;
+  webSocket.onclose = OnWebSocketClosed;
+}
+
+// --------------------------
 
 Start();
