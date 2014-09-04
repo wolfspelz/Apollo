6c76d1f7b136fcda041415c89955f0d61042b7a5
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2014-09-04 19:00:13 +0000

    Release files

--- trunk/Apollo/bin/Win32/Release/modules/WebView/apollo.js
+++ trunk/Apollo/bin/Win32/Release/modules/WebView/apollo.js
@@ -179,7 +179,7 @@ ApMessage.prototype =
     return s;
   },
 
-  _: 0
+  _avoidcommahazzle: 0
 }
 
 // ------------------------------------------
@@ -195,61 +195,59 @@ Function.prototype.bind = function(fn)
 
 function ApEval(sText) { return eval(sText); }
 
+var gApi = null;
 function ApolloApi(sModule)
 {
   this.moduleName = sModule;
+  gApi = this;
 }
 
-ApolloApi.prototype = 
+ApolloApi.prototype =
 {
   moduleName: 'Unknown',
 
-  GetViewHandle: function()
-  {
+  GetViewHandle: function () {
     return apollo.viewHandle;
   },
-  
+
   // ------------------------------------------------
   // SRPC
 
-  OnDispatchMessage: function(msg) {},
-  
-  OnReceiveMessage: function (sMsg)
-  {
+  OnDispatchMessage: function (msg) { },
+
+  OnReceiveMessage: function (sMsg) {
     Log.IO('&lt; ' + sMsg);
 
     var msg = new ApMessage().fromString(sMsg);
     var resp = null;
 
     try {
-      if (typeof(api.OnDispatchMessage) == 'function') {
-        resp = api.OnDispatchMessage(msg);
+      if (typeof (this.OnDispatchMessage) == 'function') {
+        resp = this.OnDispatchMessage(msg);
       }
     } catch (ex) {
       resp = new ApMessage().setInt('Status', 0).setString('Message', String(ex));
     }
-    
-  //  if (resp == null) {
-  //    resp = new ApMessage().setInt('Status', 1);
-  //  }
-    
+
+    //  if (resp == null) {
+    //    resp = new ApMessage().setInt('Status', 1);
+    //  }
+
     if (resp != null) {
       var sResponse = resp.toString();
       Log.IO('&nbsp;&nbsp;&gt; ' + sResponse);
       return sResponse;
     }
   },
-  
-  Message: function(sMethod)
-  {
+
+  Message: function (sMethod) {
     var msg = new ApMessage(sMethod);
-      //if (msg.getString('Method') != 'Log_Line') { api.Log.Debug(sType + ' ' + apollo.viewHandle); }
-    msg.setString('hView', apollo.viewHandle);
+    //if (msg.getString('Method') != 'Log_Line') { this.Log.Debug('apollo.js: ' + sType + ' ' + apollo.viewHandle); }
+    msg.setString('hView', this.GetViewHandle());
     return msg;
   },
 
-  ModuleCall: function(sMethod)
-  {
+  ModuleCall: function (sMethod) {
     var msg = new ApMessage(sMethod);
     msg.setString('ApType', 'WebView_ModuleCall');
     return msg;
@@ -257,12 +255,10 @@ ApolloApi.prototype =
 
   // ------------------------------------------------
   // Log
-  
-  Log: {
 
-    Send: function(nMask, sChannel, sContext, sMessage)
-    {
-      return api.Message('Log_Line')
+  Log: {
+    Send: function (nMask, sChannel, sContext, sMessage) {
+      return new ApMessage('Log_Line')
         .setInt('nMask', nMask)
         .setString('sChannel', sChannel)
         .setString('sContext', sContext)
@@ -270,38 +266,40 @@ ApolloApi.prototype =
         .send();
     },
 
-    LevelFatal  :   2,
-    LevelError  :   4,
-    LevelWarning:   8,
-    LevelUser   :  16,
-    LevelDebug  :  32,
-    LevelInfo   :  64,
+    LevelFatal: 2,
+    LevelError: 4,
+    LevelWarning: 8,
+    LevelUser: 16,
+    LevelDebug: 32,
+    LevelInfo: 64, 
     LevelVerbose: 128,
 
-    Error: function(sMessage) { api.Log.Send(api.Log.LevelError, api.moduleName, 'JS:', String(sMessage)); },
-    Warning: function(sMessage) { api.Log.Send(api.Log.LevelWarning, api.moduleName, 'JS:', String(sMessage)); },
-    User: function(sMessage) { api.Log.Send(api.Log.LevelUser, api.moduleName, 'JS:', String(sMessage)); },
-    Debug: function(sMessage) { api.Log.Send(api.Log.LevelDebug, api.moduleName, 'JS:', String(sMessage)); },
-    Info: function(sMessage) { api.Log.Send(api.Log.LevelInfo, api.moduleName, 'JS:', String(sMessage)); },
+    Error: function (sMessage) { this.Send(this.LevelError, gApi.moduleName, 'JS:', String(sMessage)); },
+    Warning: function (sMessage) { this.Send(this.LevelWarning, gApi.moduleName, 'JS:', String(sMessage)); },
+    User: function (sMessage) { this.Send(this.LevelUser, gApi.moduleName, 'JS:', String(sMessage)); },
+    Debug: function (sMessage) { this.Send(this.LevelDebug, gApi.moduleName, 'JS:', String(sMessage)); },
+    Info: function (sMessage) { this.Send(this.LevelInfo, gApi.moduleName, 'JS:', String(sMessage)); },
 
-    _:0
+    _avoidcommahazzle: 0
   },
-  
+
   // ------------------------------------------------
   // Babelfish
-  
-  TranslateElement: function(oElement, sContext)
-  {
+
+  TranslateElement: function (oElement, sContext, sModule) {
+    if (sModule == null) { sModule = this.moduleName; }
+    if (sContext == null) { sContext = ''; }
+
     if (oElement.innerText && oElement.innerText != '') {
-      Log.Debug(oElement.innerText);
-      oElement.innerText = api.Translate(oElement.innerText);
+      //Log.Debug(oElement.innerText);
+      oElement.innerText = this.Translate(oElement.innerText, sContext, sModule);
     }
-    
+
     if (oElement.tagName && oElement.tagName == 'INPUT') {
       for (var i = 0; i < oElement.attributes.length; i++) {
         if (oElement.attributes[i].name == 'type') {
           if (oElement.attributes[i].value == 'button' || oElement.attributes[i].value == 'submit') {
-            oElement.value = api.Translate(oElement.value, sContext);
+            oElement.value = this.Translate(oElement.value, sContext, sModule);
             break;
           }
         }
@@ -309,25 +307,29 @@ ApolloApi.prototype =
     }
   },
 
-  Translate: function(sText, sContext)
-  {
-    var sTranslated = api.Message('Translation_Get')
-      .setString('sModule', api.moduleName)
-      .setString('sContext', sContext)
-      .setString('sText', sText)
-      .send()
-      .getString('sTranslated');
-      
-      if (sTranslated == '') {
-        return sText;
-      } else {
-        return sTranslated;
-      }
+  // sContext may be empty
+  // sModule is optional. sModule overrides the initial new ApolloApi(moduleName)
+  Translate: function (sText, sContext, sModule) {
+    if (sModule == null) { sModule = this.moduleName; }
+    if (sContext == null) { sContext = ''; }
+
+    var msg = new ApMessage('Translation_Get');
+    msg.setString('sModule', sModule);
+    msg.setString('sContext', sContext);
+    msg.setString('sText', sText);
+    var res = msg.send();
+    var sTranslated = res.getString('sTranslated');
+
+    if (sTranslated == '') {
+      return sText;
+    } else {
+      return sTranslated;
+    }
   },
 
   // ------------------------------------------------
   // Move and size
-  
+
   nStartX: 0,
   nStartY: 0,
   bMouseActive: false,
@@ -337,30 +339,31 @@ ApolloApi.prototype =
   nSizeDirection: 8,
   nMinWidth: 100,
   nMinHeight: 100,
-  
-  OnMouseDown: function(ev)
-  {
-    api.bIsMove = false; 
-    api.bIsSize = false; 
+
+  OnMouseDown: function (ev) {
+    //this.Log.Debug('apollo.js: ' + 'OnMouseDown ' + ev.target.className + ' ' + ev.x + ' ' + ev.y);
+
+    this.bIsMove = false;
+    this.bIsSize = false;
 
     var aClasses = ev.target.className.split(' ');
     if (aClasses != null) {
       for (var i = 0; i < aClasses.length; i++) {
         switch (aClasses[i]) {
-          case 'cApMove': api.bIsMove = true; break;
-//          case 'cApSizeLeft': api.bIsSize = true; api.nSizeDirection = 1; break;
-//          case 'cApSizeTop': api.bIsSize = true; api.nSizeDirection = 2; break;
-          case 'cApSizeRight': api.bIsSize = true; api.nSizeDirection = 3; break;
-          case 'cApSizeBottom': api.bIsSize = true; api.nSizeDirection = 4; break;
-//          case 'cApSizeTopLeft': api.bIsSize = true; api.nSizeDirection = 5; break;
-//          case 'cApSizeTopRight': api.bIsSize = true; api.nSizeDirection = 6; break;
-//          case 'cApSizeBottomLeft': api.bIsSize = true; api.nSizeDirection = 7; break;
-          case 'cApSizeBottomRight': api.bIsSize = true; api.nSizeDirection = 8; break;
+          case 'cApMove': this.bIsMove = true; break;
+          //          case 'cApSizeLeft': this.bIsSize = true; this.nSizeDirection = 1; break;       
+          //          case 'cApSizeTop': this.bIsSize = true; this.nSizeDirection = 2; break;       
+          case 'cApSizeRight': this.bIsSize = true; this.nSizeDirection = 3; break;
+          case 'cApSizeBottom': this.bIsSize = true; this.nSizeDirection = 4; break;
+          //          case 'cApSizeTopLeft': this.bIsSize = true; this.nSizeDirection = 5; break;       
+          //          case 'cApSizeTopRight': this.bIsSize = true; this.nSizeDirection = 6; break;       
+          //          case 'cApSizeBottomLeft': this.bIsSize = true; this.nSizeDirection = 7; break;       
+          case 'cApSizeBottomRight': this.bIsSize = true; this.nSizeDirection = 8; break;
         }
       }
     }
-    
-    if (api.bIsMove || api.bIsSize) {    
+
+    if (this.bIsMove || this.bIsSize) {
       //<div id="Cover" style=";left:8px;top:24px;right:8px;bottom:8px;background-color:#FFFFFF;opacity:0.01;z-index:99;"></div>
       var eDiv = document.createElement('DIV');
       eDiv.setAttribute('id', 'cApCover');
@@ -374,50 +377,51 @@ ApolloApi.prototype =
       eDiv.style.opacity = '0.01';
       eDiv.style.zIndex = '99';
       document.body.appendChild(eDiv);
-      
-      api.Message('WebView_MouseCapture').send();
-      api.nStartX = ev.x;
-      api.nStartY = ev.y;
-      api.bMouseActive = false;
-      api.bMouseCaptured = true;
+
+      this.Message('WebView_MouseCapture').send();
+      this.nStartX = ev.x;
+      this.nStartY = ev.y;
+      this.bMouseActive = false;
+      this.bMouseCaptured = true;
     }
 
     return false;
   },
 
-  OnMouseMove: function(ev)
-  {
-    if (api.bMouseCaptured) {
-      var dx = ev.x - api.nStartX;
-      var dy = ev.y - api.nStartY;
-      if (!api.bMouseActive) {
+  OnMouseMove: function (ev) {
+    if (!this.bMouseCaptured) {
+      //this.Log.Debug('apollo.js: ' + 'OnMouseMove');
+    } else {
+      var dx = ev.x - this.nStartX;
+      var dy = ev.y - this.nStartY;
+      if (!this.bMouseActive) {
         if (dx > 4 || dx < -4 || dy > 4 || dy < -4) {
-          api.bMouseActive = true;
+          this.bMouseActive = true;
         }
       }
-      if (api.bMouseActive) {
-        if (api.bIsMove) {
-          api.Message('WebView_MoveBy').setInt('nX', dx).setInt('nY', dy).send();
+      if (this.bMouseActive) {
+        if (this.bIsMove) {
+          this.Message('WebView_MoveBy').setInt('nX', dx).setInt('nY', dy).send();
         }
-        if (api.bIsSize) {
+        if (this.bIsSize) {
           if (dx < 0 || dy < 0) {
-            var pos = api.Message('WebView_GetPosition').send();
+            var pos = this.Message('WebView_GetPosition').send();
             var nW = pos.getInt('nWidth');
             var nH = pos.getInt('nHeight');
             var nNewW = nW + dx;
             var nNewH = nH + dy;
-            if (nNewW < api.nMinWidth) { dx = api.nMinWidth - nW; }
-            if (nNewH < api.nMinHeight) { dy = api.nMinHeight - nH; }
+            if (nNewW < this.nMinWidth) { dx = this.nMinWidth - nW; }
+            if (nNewH < this.nMinHeight) { dy = this.nMinHeight - nH; }
           }
 
-          api.Message('WebView_SizeBy')
+          this.Message('WebView_SizeBy')
             .setInt('nX', dx)
             .setInt('nY', dy)
-            .setInt('nDirection', api.nSizeDirection)
+            .setInt('nDirection', this.nSizeDirection)
             .send();
 
-          api.nStartX += dx;
-          api.nStartY += dy;
+          this.nStartX += dx;
+          this.nStartY += dy;
         }
       }
     }
@@ -425,33 +429,31 @@ ApolloApi.prototype =
     return false;
   },
 
-  OnMouseUp: function()
-  {
+  OnMouseUp: function () {
+    //this.Log.Debug('apollo.js: ' + 'OnMouseUp');
     if (document.getElementById('cApCover')) {
       document.body.removeChild(document.getElementById('cApCover'));
     }
-    
-    api.bMouseCaptured = false;
-    api.bMouseActive = false;
-    api.bIsMove = false;
-    api.bIsSize = false;
-    api.Message('WebView_MouseRelease').send();
+
+    this.bMouseCaptured = false;
+    this.bMouseActive = false;
+    this.bIsMove = false;
+    this.bIsSize = false;
+    this.Message('WebView_MouseRelease').send();
 
     return false;
   },
 
-  InitMoveSize: function()
-  {
-    document.onmousedown = api.OnMouseDown.bind(this); 
-    document.onmousemove = api.OnMouseMove.bind(this); 
-    document.onmouseup = api.OnMouseUp.bind(this);
+  InitMoveSize: function () {
+    document.onmousedown = this.OnMouseDown.bind(this);
+    document.onmousemove = this.OnMouseMove.bind(this);
+    document.onmouseup = this.OnMouseUp.bind(this);
   },
 
   // ------------------------------------------------
-  
-  Init: function()
-  {
+
+  Init: function () {
   },
 
-  _:0
+  _avoidcommahazzle: 0
 }
