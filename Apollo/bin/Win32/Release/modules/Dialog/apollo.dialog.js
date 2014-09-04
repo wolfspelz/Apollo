a18baad64d37aa14c9f7c07bbda50e01f2b9fefa
Heiner Wolf <wolfspelz@users.noreply.github.com>
2014-09-04 19:00:13 +0000

    Release files

--- Apollo/bin/Win32/Release/modules/Dialog/apollo.dialog.js
+++ Apollo/bin/Win32/Release/modules/Dialog/apollo.dialog.js
@@ -16,6 +16,31 @@ ApSetIcon = function(sUrl)
   $('#iApIcon').attr('src', sUrl);
 }
 
+ApGetContentRect = function ()
+{
+  var nLeft = 0;
+  var nTop = 0;
+  var nWidth = 0;
+  var nHeight = 0;
+
+  var e = document.getElementById('iApContent');
+  nWidth = e.offsetWidth;
+  nHeight = e.offsetHeight;
+
+  while (e != null) {
+    nLeft += e.offsetLeft;
+    nTop += e.offsetTop;
+    e = e.offsetParent;
+  }
+
+  var msg = new ApMessage();
+  msg.setInt('nLeft', nLeft);
+  msg.setInt('nTop', nTop);
+  msg.setInt('nWidth', nWidth);
+  msg.setInt('nHeight', nHeight);
+  return msg.toString();
+}
+
 //ApContentEval = function(sCode)
 //{
 //  return document.getElementById('iApContent').contentWindow.ApEval(sCode);
