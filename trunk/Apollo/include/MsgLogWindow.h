0f63d67efaad4aa9668d888ef581bf1c6b887fc9
wolf.heiner@gmail.com <wolf.heiner@gmail.com@a144e2aa-23a6-ffb2-3e72-e31fd13106b3>
2014-09-10 07:49:19 +0000

    LogWindow: using Config_SetValue instead of LogWindow_SetFilterMask is more general, eliminates a message type, and reduces code which is always better. More general, smaller code, i love it.

--- trunk/Apollo/include/MsgLogWindow.h
+++ trunk/Apollo/include/MsgLogWindow.h
@@ -14,7 +14,6 @@ class Msg_LogWindow_Open: public ApRequestMessage
 {
 public:
   Msg_LogWindow_Open() : ApRequestMessage("LogWindow_Open") {}
-  ApIN ApHandle hLogWindow;
 };
 
 // -> LogWindow
@@ -22,16 +21,6 @@ class Msg_LogWindow_Close: public ApRequestMessage
 {
 public:
   Msg_LogWindow_Close() : ApRequestMessage("LogWindow_Close") {}
-  ApIN ApHandle hLogWindow;
-};
-
-// -> LogWindow
-class Msg_LogWindow_SetFilterMask: public ApRequestMessage
-{
-public:
-  Msg_LogWindow_SetFilterMask() : ApRequestMessage("LogWindow_SetFilterMask"), nMask(0) {}
-  ApIN ApHandle hLogWindow;
-  ApIN int nMask;
 };
 
 #endif // !defined(MsgLogWindow_h_INCLUDED)
